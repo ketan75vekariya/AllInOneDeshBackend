8790d670ed791f93b7d48da07e511f22de5cdd92
ketan75vekariya <ketan75vekariya@yahoo.com>
2025-01-16 12:35:12 +0000

    Added project store functionality

---
+++ app/Http/Controllers/ProjectController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\DB;
+use App\Models\Project;
+
+use Str;
+
+class ProjectController extends Controller
+{
+    public function store(Request $request)
+    {
+
+        return DB::transaction(function () use ($request) {
+            $fields = $request->all();
+
+            $errors = Validator::make($fields, [
+                'name' => 'required',
+                // 'status' => 'required',
+                'startDate' => 'required',
+                'endDate' => 'required',
+            ]);
+
+            if ($errors->fails()) {
+                return response($errors->errors()->all(), 422);
+            }
+
+            $project = Project::create([
+                'name' => $fields['name'],
+                'startDate' => $fields['startDate'],
+                'endDate' => $fields['endDate'],
+                'status' => Project::NOT_STARTED,
+                'slug' => Project::createSlug($fields['name'])
+
+            ]);
+
+            return response(['message' => 'project created'], 200);
+        });
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProjectController;
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/project', [ProjectController::class, 'store']);
